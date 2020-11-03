37cc17455d8562e3c6684bad464ecf828c74d3f8
andron13 <andron13@gmail.com>
2020-11-03 10:13:54 +0100

    Git intro

--- java-modul2/src/Main.java
+++ java-modul2/src/Main.java
@@ -5,5 +5,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет мир!");
+        String hm = "Василий";
+        String str1 = hm.substring(4);
     }
 }
---
+++ justExample/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * 14m-modul2-mono-rep
+ * 03 10 : 06
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
