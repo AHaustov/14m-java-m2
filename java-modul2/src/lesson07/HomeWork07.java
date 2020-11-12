5a315e8041b2f3e4a6a9b8e8ee931f963e92f6fa
AHaustov <anton.haust@gmail.com>
2020-11-12 10:53:52 +0100

    HomeWork 07 fixed

--- java-modul2/src/lesson07/HomeWork07.java
+++ java-modul2/src/lesson07/HomeWork07.java
@@ -7,17 +7,18 @@ public class HomeWork07 {
         System.out.println(Arrays.toString(fix34(1, 3, 1, 4)));// → [1, 3, 4, 1]
         System.out.println(Arrays.toString(fix34(1, 3, 1, 4, 4, 3, 1)));// → [1, 3, 4, 1, 1, 3, 4]
         System.out.println(Arrays.toString(fix34(3, 2, 2, 4)));// → [3, 4, 2, 2]
+        System.out.println(Arrays.toString(fix34(1, 3, 2, 4, 4, 3, 1, 3, 1, 3, 4, 2, 2, 1, 3, 2, 4, 4, 3, 1, 4)));// → [3, 4, 2, 2]
     }
 
     private static int[] fix34(int... args) {
-        int[] output = new int[args.length];
+        //int[] output = new int[args.length];
         for (int i = 0; i < args.length; i++) {
-            output[i] = args[i];
+            //output[i] = args[i];
             if (args[i] == 3) {
-                for (int j = args.length - 1; 0 < j; j--) {
-                    if (args[j] == 4) {
+                for (int j = args.length - 1; 0 <= j; j--) {
+                    if (((j > 0 && args[j - 1] != 3) || j == 0) && args[j] == 4) {
                         int temp = args[i + 1];
-                        output[i + 1] = args[j];
+                        //output[i + 1] = args[j];
                         args[i + 1] = args[j];
                         args[j] = temp;
                         i++;
@@ -26,6 +27,6 @@ public class HomeWork07 {
                 }
             }
         }
-        return output;
+        return args;
     }
 }
